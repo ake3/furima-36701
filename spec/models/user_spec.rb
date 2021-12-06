a2bea0b375a8826531ccb9c7a75052d8b9f63af1
ake3 <kita6119@gmail.com>
2021-12-06 21:42:55 +0900

    指摘箇所の訂正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -93,6 +93,12 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
       end
+      it 'passwordが全角では登録できない' do
+        @user.password = 'ａｂ１２３４'
+        @user.password_confirmation = 'ａｂ１２３４'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = 'abc123'
         @user.password_confirmation = 'cdf456'
