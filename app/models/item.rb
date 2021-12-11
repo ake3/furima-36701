079d1f74763777da5b402dbca63397c436c20ff4
ake3 <kita6119@gmail.com>
2021-12-11 11:10:08 +0900

    バリデーション・アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :user
+
+  validates :name, :explanation, :selling_price, presence: true
+  validates :selling_price, 
+            format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' },
+            inclusion: { in: 300..9999999, message: 'is out of setting range' }
+  validates :category_id, :status_id, :shipping_cost_id, 
+            :prefecture_id, :shipping_date_id, 
+            numericality: { other_than: 1 , message: 'can’t be blank' }
 end
