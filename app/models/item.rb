93ad671c03112cffffac8c5dbc5d3fbcd0e350d4
ake3 <kita6119@gmail.com>
2021-12-13 23:24:31 +0900

    itemモデルテストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,10 +9,9 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :image, :name, :explanation, :selling_price, presence: true
-  validates :selling_price, 
-            format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' },
-            inclusion: { in: 300..9999999, message: 'is out of setting range' }
+  validates :selling_price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' },
+                            inclusion: { in: 300..9999999, message: 'is out of setting range' }
   validates :category_id, :status_id, :shipping_cost_id, 
             :prefecture_id, :shipping_date_id, 
-            numericality: { other_than: 1 , message: 'can’t be blank' }
+            numericality: { other_than: 1 , message: "can't be blank" }
 end
